f234ea3311e91e69d75a2e862b17fc83c3352875
LauraPlaza <laurita.infor@gmail.com>
2019-04-25 13:22:23 +0200

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# EjemploCI
-Created with CodeSandbox
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>Hello Clase!</h1>
+<div>
+  Esta es la home de un proyecto básico<br/>
+  <a href="https://aulavirtual33.educa.madrid.org/ies.elcanaveral.mostoles/course/view.php?id=163" target="_blank" rel="noopener noreferrer">here</a>.
+</div>
+`;
